d58d6295093cb08f406b3297a026b90f1bc617e6
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-08 23:29:36 +0900

    Deploy 2025-10-08 23:29:36

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-      base: '/sincerity-dev/',
+      base: '/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
